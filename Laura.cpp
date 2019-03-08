448458bb98936d24477a611bbfa563c08422aec4
Adrian Gonzalez <adrian_gonz@live.com>
2019-03-07 20:55:40 -0700

    Prints Laura

---
+++ Laura.cpp
@@ -0,0 +1,16 @@
+// Print Laura
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "L      A     U     U RRRRR     A"   << endl;
+	cout << "L     A A    U     U R   R    A A"  << endl;
+	cout << "L    A   A   U     U R  R    A   A" << endl;
+	cout << "LLL A     A   UUUUU  R   R  A     A"<< endl;
+	cout << endl;
+	
+	//system("pause");
+	return 0;
+}
